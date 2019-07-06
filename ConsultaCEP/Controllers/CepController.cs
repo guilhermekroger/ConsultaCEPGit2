edbd4e13a0e01bedf01c59766481a09782567550
Lucas Leonardo <leus12pa@hotmail.com>
2019-07-06 15:14:24 -0300

    Add project files.

---
+++ ConsultaCEP/Controllers/CepController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using ConsultaCEP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+
+namespace ConsultaCEP.Controllers
+{
+    public class CepController : Controller
+    {
+        private readonly Context _context;
+
+         public CepController(Context context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(_context.Ceps.ToList());
+        }
+
+        public IActionResult ConsultarCep(CEP Ceps)
+        {
+            WebClient client = new WebClient();
+            string json = client.DownloadString("https://viacep.com.br/ws/"+Ceps.Cep+"/json/");
+            CEP cep = JsonConvert.DeserializeObject<CEP>(json);
+            _context.Ceps.Add(cep);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
